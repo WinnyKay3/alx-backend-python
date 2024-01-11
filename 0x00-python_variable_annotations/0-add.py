98b386eba196300e71410bc72b742473df876c09
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-11 15:21:53 +0000

    0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,11 +2,6 @@
 '''a module containing the add function'''
 def add(a: float, b: float) -> float:
     '''a function that takes float a and b as arguments
-    Args: 
-       a (float): the first number to be added
-       b (float): the second number to be added
-
-    Returns: 
-        float: The sum of a and b
+    and returns the sum of the two values
     '''
     return a + b
