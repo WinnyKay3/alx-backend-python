193755ccbfcec929cb12ad1e08602d1bfb404309
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-11 15:23:49 +0000

    0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 '''a module containing the add function'''
+
+
 def add(a: float, b: float) -> float:
     '''a function that takes float a and b and return the sum'''
     return a + b
