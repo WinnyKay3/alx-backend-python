2dd421ae78c41687d24343bf9afe5d89392c1d87
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-12 06:37:32 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Calculate the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return int(n)
