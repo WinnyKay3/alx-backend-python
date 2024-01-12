9847f60c2c906665e827700ab6fc069d1b557833
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-12 06:36:14 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''concancates a string'''
+
+
+def concat(str1: string, str2: string) -> string:
+     ''' Concatenate two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+     '''
+     return str1 + str2
+        
